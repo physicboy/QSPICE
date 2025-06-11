adfaaf1f0866800b5ef774081f9c87ed99609110
physicboy <noorrahman.arief@gmail.com>
2025-06-12 00:38:21 +0800

    move old FRA to Archive and upload the newer FRA-V3
    move old FRA to Archive and upload the newer FRA-V3

---
+++ FRA_project/FRA-V3/fra_v3.cpp
@@ -0,0 +1,345 @@
+// Automatically generated C++ file on Wed Oct 16 12:21:52 2024
+//
+// To build with Digital Mars C++ Compiler:
+//
+//    dmc -mn -WD fra_v3.cpp kernel32.lib
+
+#include <malloc.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+extern "C" __declspec(dllexport) int (*Display)(const char *format, ...) = 0; // works like printf()
+extern "C" __declspec(dllexport) const double *DegreesC                  = 0; // pointer to current circuit temperature
+extern "C" __declspec(dllexport) const int *StepNumber                   = 0; // pointer to current step number
+extern "C" __declspec(dllexport) const int *NumberSteps                  = 0; // pointer to estimated number of steps
+extern "C" __declspec(dllexport) const char **InstanceName               = 0; // pointer to address of instance name
+extern "C" __declspec(dllexport) const char *QUX                         = 0; // path to QUX.exe
+extern "C" __declspec(dllexport) int (*DFFT)(struct sComplex *u, bool inv, unsigned int N, double scale) = 0;
+
+union uData
+{
+   bool b;
+   char c;
+   unsigned char uc;
+   short s;
+   unsigned short us;
+   int i;
+   unsigned int ui;
+   float f;
+   double d;
+   long long int i64;
+   unsigned long long int ui64;
+   char *str;
+   unsigned char *bytes;
+};
+
+// int DllMain() must exist and return 1 for a process to load the .DLL
+// See https://docs.microsoft.com/en-us/windows/win32/dlls/dllmain for more information.
+int __stdcall DllMain(void *module, unsigned int reason, void *reserved) { return 1; }
+
+void bzero(void *ptr, unsigned int count)
+{
+   unsigned char *first = (unsigned char *) ptr;
+   unsigned char *last  = first + count;
+   while(first < last)
+      *first++ = '\0';
+}
+
+// #undef pin names lest they collide with names in any header file(s) you might include.
+#undef sw_trig
+#undef sense
+#undef IN1A
+#undef IN1B
+#undef IN2A
+#undef IN2B
+#undef amp
+#undef freq
+#undef param1
+#undef param2
+#undef param3
+#undef param4
+
+struct fra_data
+{
+   double i;
+   double freq;
+   double mag;
+
+   double ts;
+   double tsampling;
+   double tstep;
+
+   double fmin;
+   double fmax;
+   double alo;
+   double ahi;
+   double flo;
+   double fhi;
+   int    fstep;
+
+   bool lin0_log1;
+
+   double dwell_mintime;
+   double dwell_period;
+   double meas_mintime;
+   double meas_period;
+
+   double in1ap;
+   double in1bp;
+   double in2ap;
+   double in2bp;
+
+   double in1ph;
+   double in2ph;
+   double in1wrap;
+   double in2wrap;
+};
+
+struct sFRA_V3
+{
+   // declare the structure here
+   int   status;
+   struct fra_data fra;
+
+   double maxstep;
+   double t_prev;
+
+   bool plot;
+};
+
+void SS_DETECT(struct ss_data *s, double *t,
+   double *t_prev, double *sense, double *sw_trig,
+   double *param1, double *param2, double *param3, double *param4);
+void FRA_CORE(struct fra_data *f, double *t,
+      double *in1a, double *in1b, double *in2a, double *in2b);
+
+FILE *fptr;
+char *fname = "fra.csv";
+
+extern "C" __declspec(dllexport) void fra_v3(struct sFRA_V3 **opaque, double t, union uData *data)
+{
+   double  IN1A          = data[ 0].d; // input
+   double  IN1B          = data[ 1].d; // input
+   double  IN2A          = data[ 2].d; // input
+   double  IN2B          = data[ 3].d; // input
+   double  param1        = data[ 4].d; // input
+   double  param2        = data[ 5].d; // input
+   double  param3        = data[ 6].d; // input
+   double  param4        = data[ 7].d; // input
+   double  f_min         = data[ 8].d; // input parameter
+   double  f_max         = data[ 9].d; // input parameter
+   double  a_lo          = data[10].d; // input parameter
+   double  a_hi          = data[11].d; // input parameter
+   double  f_lo          = data[12].d; // input parameter
+   double  f_hi          = data[13].d; // input parameter
+   bool    lin0_log1     = data[14].b; // input parameter
+   double  tstep_factor  = data[15].d; // input parameter
+   double  ss_tmax       = data[16].d; // input parameter
+   double  dwell_mintime = data[17].d; // input parameter
+   double  dwell_period  = data[18].d; // input parameter
+   double  meas_mintime  = data[19].d; // input parameter
+   double  meas_period   = data[20].d; // input parameter
+   int     f_step        = data[21].i; // input parameter
+   bool    plot          = data[22].b; // input parameter
+   double &amp           = data[23].d; // output
+   double &freq          = data[24].d; // output
+
+   if(!*opaque)
+   {
+      *opaque = (struct sFRA_V3 *) malloc(sizeof(struct sFRA_V3));
+      bzero(*opaque, sizeof(struct sFRA_V3));
+
+      struct sFRA_V3 *inst = *opaque;
+
+      inst->fra.fmin = f_min;
+      inst->fra.fmax = f_max;
+      inst->fra.alo  = a_lo;
+      inst->fra.ahi  = a_hi;
+      inst->fra.flo  = f_lo;
+      inst->fra.fhi  = f_hi;
+      inst->fra.lin0_log1 = lin0_log1;
+      inst->fra.fstep = f_step;
+      inst->fra.dwell_mintime = dwell_mintime;
+      inst->fra.dwell_period = dwell_period;
+      inst->fra.meas_mintime = meas_mintime;
+      inst->fra.meas_period = meas_period;
+
+      inst->plot = plot;
+   }
+   struct sFRA_V3 *inst = *opaque;
+
+// Implement module evaluation code here:
+   if(inst->status == 0)
+   {
+      if(t > ss_tmax)inst->status = 1;
+      inst->fra.i = -0.5;
+
+      amp = inst->fra.i;
+      freq = inst->fra.freq;
+
+      if(inst->status == 1)
+      {
+         Display("\ti\tfreq\tmag(in2/in1)[dB]\targ(in2/in1)[deg]\tmag(in1)[dB]\targ(in1)[deg]\tmag(in2)[dB]\targ(in2)[deg]\n");
+
+         fptr = fopen(fname,"w");
+         fprintf(fptr,"Frequency,IN2/IN1\n");
+         fclose(fptr);
+      }
+   }
+   else
+   {
+      FRA_CORE(&inst->fra, &t, &IN1A, &IN1B, &IN2A, &IN2B);
+
+      amp = inst->fra.mag;
+      freq = inst->fra.freq;
+
+
+      // Timing control ===============================================
+      if(inst->fra.i >= inst->fra.fstep)inst->maxstep = -1e+308;
+      else
+      {
+         inst->maxstep = inst->fra.tsampling - t;
+         if(inst->maxstep > tstep_factor/inst->fra.freq)
+         {
+            inst->maxstep = tstep_factor/inst->fra.freq;
+         }
+      }
+   }
+   inst->t_prev = t;
+
+}
+
+void FRA_CORE(struct fra_data *f, double *t, double *in1a, double *in1b, double *in2a, double *in2b)
+{
+   if(*t >= f->tsampling)
+   {
+      f->i = f->i + 0.5;
+      double ts;
+      double fx;
+      double freq_p = f->freq;
+      if(f->i == floor(f->i))
+      {
+         if(f->lin0_log1)
+         {
+            // injection frequency computation in log space
+            f->freq = f->fmin*pow(10,log10(f->fmax/f->fmin)*floor(f->i)/((double)f->fstep-1));
+
+            // injection frequency computation in log space
+            if(f->freq > f->fhi)f->mag = f->ahi;
+            else
+            {
+               if(f->freq < f->flo)f->mag = f->alo;
+               else f->mag = f->alo + (f->ahi - f->alo)*(log10(f->freq) - log10(f->flo))/(log10(f->fhi) - log10(f->flo));
+            }
+         }
+         else
+         {
+            // injection frequency computation in linear space
+            f->freq = f->fmin + (f->fmax - f->fmin)*floor(f->i)/((double)f->fstep-1);
+
+            // injection amplitude computation in linear space
+            if(f->freq > f->fhi)f->mag = f->ahi;
+            else
+            {
+               if(f->freq < f->flo)f->mag = f->alo;
+               else f->mag = f->alo + (f->ahi - f->alo)*(f->freq - f->flo)/(f->fhi - f->flo);
+            }
+         }
+         ts = f->dwell_period/f->freq;
+         if(ts < f->dwell_mintime) ts = f->dwell_mintime;
+         f->tsampling = *t + ts;
+      }
+      else
+      {
+         ts = f->meas_period/f->freq;
+         if(ts < f->meas_mintime) ts = ceil(f->meas_mintime*f->freq)/f->freq;
+         f->tsampling = *t + ts;
+      }
+      //============================================================================
+      if(f->i > 0)
+      {
+         if(f->i != floor(f->i))
+         {
+            f->in1ap = *in1a;
+            f->in1bp = *in1b;
+            f->in2ap = *in2a;
+            f->in2bp = *in2b;
+         }
+         else
+         {
+            double in1am, in1bm, in2am, in2bm;
+            double in1mag, in1ph, in2mag, in2ph;
+            double in21mag, in21ph;
+            in1am = 2/f->ts*(*in1a - f->in1ap);
+            in1bm = 2/f->ts*(*in1b - f->in1bp);
+            in2am = 2/f->ts*(*in2a - f->in2ap);
+            in2bm = 2/f->ts*(*in2b - f->in2bp);
+
+            in1mag = 20*log10(sqrt(in1am*in1am + in1bm*in1bm));
+            in1ph  = 180/M_PI*atan2(in1bm,in1am);
+            in2mag = 20*log10(sqrt(in2am*in2am + in2bm*in2bm));
+            in2ph  = 180/M_PI*atan2(in2bm,in2am);
+
+            in1ph = in1ph + f->in1wrap;
+            in2ph = in2ph + f->in2wrap;
+
+            if(in1ph > (f->in1ph + 1800))
+            {
+               f->in1wrap -= 360;
+               in1ph += f->in1wrap;
+            }
+            else
+            {
+               if(in1ph < (f->in1ph - 180))
+               {
+                  f->in1wrap += 360;
+                  in1ph += f->in1wrap;
+               }
+            }
+
+            if(in2ph > (f->in2ph + 270))
+            {
+               f->in2wrap -= 360;
+               in2ph += f->in2wrap;
+            }
+            else
+            {
+               if(in2ph < (f->in2ph - 270))
+               {
+                  f->in2wrap += 360;
+                  in2ph += f->in2wrap;
+               }
+            }
+
+            f->in1ph = in1ph;
+            f->in2ph = in2ph;
+
+            in21mag = in2mag - in1mag;
+            in21ph = in2ph - in1ph;
+            Display("\t%d\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n",(int)f->i,freq_p,in21mag,in21ph,in1mag,in1ph,in2mag,in2ph);
+
+            double re,im;
+            re = pow(10,in21mag/20)*cos(in21ph*M_PI/180);
+            im = pow(10,in21mag/20)*sin(in21ph*M_PI/180);
+
+            fptr = fopen(fname,"a");
+            fprintf(fptr,"%f\t%f,%f\n",freq_p,re,im);
+            fclose(fptr);
+         }
+      }
+      f->ts = ts;
+   }
+}
+
+extern "C" __declspec(dllexport) double MaxExtStepSize(struct sFRA_V3 *inst)
+{
+   return inst->maxstep; // implement a good choice of max timestep size that depends on struct sFRA_V3
+}
+
+extern "C" __declspec(dllexport) void Destroy(struct sFRA_V3 *inst)
+{
+   if(inst->plot) system("\"c:\\Program Files\\QSPICE\\QUX\" fra.csv");
+
+   free(inst);
+}
