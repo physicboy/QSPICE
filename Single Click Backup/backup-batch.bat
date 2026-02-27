4d2fc0241066352820ad76c4e6995748f1c07d2b
physicboy <noorrahman.arief@gmail.com>
2026-02-28 03:26:10 +0800

    Update backup-batch.bat

--- Single Click Backup/backup-batch.bat
+++ Single Click Backup/backup-batch.bat
@@ -1,4 +1,14 @@
 @echo off
+:: Check for admin rights
+net session >nul 2>&1
+if %errorlevel% NEQ 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process '%~f0' -Verb runAs"
+    exit /b
+)
+
+echo Running as administrator!
+
 setlocal
 
 REM === TARGET FOLDER ===
