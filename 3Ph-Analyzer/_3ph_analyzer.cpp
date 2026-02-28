02d2732611f481c913f1d8068b2de85fcb8890c6
physicboy <noorrahman.arief@gmail.com>
2026-02-28 19:37:44 +0800

    add apache license on the source code

--- 3Ph-Analyzer/_3ph_analyzer.cpp
+++ 3Ph-Analyzer/_3ph_analyzer.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2026 Arief Noor Rahman - Power Control Design
+ * 
+ * Project  : 3Ph-Analyzer
+ * Filename : _3ph_analyzer.cpp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Automatically generated C++ file on Wed Dec 17 09:20:16 2025
 //
 // To build with Digital Mars C++ Compiler:
@@ -23,7 +42,7 @@ extern "C" __declspec(dllexport) const int *IntegrationOrder                    
 extern "C" __declspec(dllexport) const char *InstallDirectory                   = 0;
 extern "C" __declspec(dllexport) double (*EngAtof)(const char **string)         = 0;
 extern "C" __declspec(dllexport) const char *(*BinaryFormat)(unsigned int data)                          = 0; // BinaryFormat(0x1C) returns "0b00011100"
-extern "C" __declspec(dllexport) const char *(*EngFormat   )(double x, const char *units, int numDgts)   = 0; // EngFormat(1e-6, "s", 6) returns "1µs"
+extern "C" __declspec(dllexport) const char *(*EngFormat   )(double x, const char *units, int numDgts)   = 0; // EngFormat(1e-6, "s", 6) returns "1ï¿½s"
 extern "C" __declspec(dllexport) int (*DFFT)(struct sComplex *u, bool inv, unsigned int N, double scale) = 0; // Discrete Fast Fourier Transform
 extern "C" __declspec(dllexport) void (*bzero)(void *ptr, unsigned int count)   = 0;
 
