086c31e61911224b6205d6d5771dc95b9c4efe6c
physicboy <noorrahman.arief@gmail.com>
2026-02-28 19:37:19 +0800

    add apache license on the source code

--- 3P4L SVPWM Interleaved/inv_controller.cpp
+++ 3P4L SVPWM Interleaved/inv_controller.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2026 Arief Noor Rahman - Power Control Design
+ * 
+ * Project  : 3P4L SVPWM Interleaved
+ * Filename : inv_controller.cpp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Automatically generated C++ file on Fri May  2 09:51:57 2025
 //
 // To build with Digital Mars C++ Compiler:
